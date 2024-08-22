62384cd8bbee21e25e093e145d059f9567ad61b5
kut2tjjj <94945420+kut2tjjj@users.noreply.github.com>
2024-08-22 21:33:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -218,9 +218,9 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
+                         return new Response('Not found', { status: 404 });
                         // www.speedtest.net
-                        url.hostname = '';
+                        url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
